7e623c76c7155089d6e777160c4dd1959370b405
czb420322 <2751470984@qq.com>
2023-03-01 21:59:36 +0800

    提交动画效果

--- src/charts.vue
+++ src/charts.vue
@@ -25,15 +25,39 @@
       </div>
     </div>
   </div>
+  <div style="border:1px solid red;">
+    <el-button @click=handleShow type="button" id="id-button-show">显示</el-button>
+    <el-button @click="handleHide" type="button" id="id-button-hide">隐藏</el-button>
+    <h3 class="h3-text">jQuery 效果 - 隐藏和显示</h3>
+  </div>
+  <div>
+    <div class='panel'>
+      <p>Eiege.com</p>
+      <p>jquery专栏，学习和交流。Eiege专栏</p>
+    </div>
+    <p class='flip' @click="huadong">请点击这里</p>
+  </div>
 </template>
 
 <script setup>
-import { reactive, nextTick } from "vue";
+import $ from 'jquery'
+import { reactive, nextTick, getCurrentInstance } from "vue";
+const { proxy } = getCurrentInstance()
+const flag = ref(false)
 const state = reactive({ count: 0 });
 const obj = reactive({
   nested: { count: 0 },
   arr: ['foo', 'bar']
 })
+const handleShow = () => {
+  $('.h3-text').show()
+}
+const handleHide = () => {
+  $('.h3-text').hide()
+}
+const huadong = () => {
+  $('.panel').slideToggle('slow');
+}
 function mutateDeeply() {
   obj.nested.count++;
   obj.arr.push("baz")
@@ -85,4 +109,19 @@ function increment() {
       }
     }
   }
-}</style>
+}
+
+.panel,
+p.flip {
+  margin: 0px;
+  padding: 5px;
+  text-align: center;
+  background: #e5eecc;
+  border: solid 1px #c3c3c3;
+}
+
+.panel {
+  height: 120px;
+  display: none;
+}
+</style>
